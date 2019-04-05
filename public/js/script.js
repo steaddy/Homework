3eb8f104399562cfe1d8d3fd5b51d0be674b2c4d
steaddy <9254497089@mail.ru>
2019-04-05 15:21:54 +0300

    Lesson 6

--- public/js/script.js
+++ public/js/script.js
@@ -20,92 +20,111 @@ class ItemList {
     }
 
     fetchItems() {
-        return new Promise((resolve, reject) => {
-            sendRequest(`${API_URL}/catalogData.json`).then(answer => {
-                this.items = answer;
-                resolve();
-            });
-        });
+        return newSendRequest(`http://localhost:3000/products.json`).then((answer) => {
+            this.items = answer.map(arrayElement => new Item(arrayElement.name, arrayElement.price));
+            //return this.items;
+        })
     }
+
 
     render() {
         let listHtml = '';
+
         this.items.forEach(item => {
-            const newItem = new Item(item.product_name, item.price);
-            listHtml += newItem.render();
+            listHtml += item.render();
         });
         document.querySelector('.goods').innerHTML = listHtml;
     }
-
 }
-/*
+
+
 
 let itemList = new ItemList;
-itemList.fetchItems().then(() => {
-    itemList.render();
-});
-*/
+itemList.fetchItems().then(() => itemList.render());
 
 
-function sendRequest(url) {
+function newSendRequest(url) {
     return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.send();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4) {
-                resolve(JSON.parse(xhr.responseText));
+        let xhr1 = new XMLHttpRequest();
+        xhr1.open('GET', url);
+        xhr1.send();
+        xhr1.onreadystatechange = () => {
+            if (xhr1.readyState === XMLHttpRequest.DONE) {
+                if (xhr1.status === 200) {
+                    resolve((JSON.parse(xhr1.responseText)));
+                } else {
+                    reject();
+                }
             }
-        }
+        };
     })
 }
 
 
-let butt = document.querySelector('.cartButton');
-butt.addEventListener('click', () => {
-    sendRequest(`${API_URL}/catalogData.json`).then((phones) => {
-        let phonesString = phones.map(item => `<li>${item.name}: ${item.number}</li>`).join('');
-        document.getElementById('phones').innerHTML = "<ul>" + phonesString + "</ul>";
-    }/*, onRejected => console.log(onRejected)   */)
-})
+// Пример с Fetch()
+
+
+let list = document.querySelector('#phones ul');
+
+fetch(`http://localhost:3000/phones.json`)
+    .then((response) => response.json())
+    .then((response) => {
+        list.innerHTML = response.map(item =>
+            `<li>Name: ${item.name}, number: ${item.number}.</li>`
+        ).join('')
+    })
 
 
 
 
 
 
-// Lesson 5
-
-let app = new Vue({
-    el: '#app',
-    data: {
-        goods: [],
-        filteredGoods: [],
-        searchLine: '',
-    },
-    methods: {
-
-        sendRequest(url) {
-            return new Promise((resolve, reject) => {
-                let xhr = new XMLHttpRequest();
-                xhr.open('GET', url, true);
-                xhr.send();
-                xhr.onreadystatechange = () => {
-                    if (xhr.readyState === 4) {
-                        resolve(JSON.parse(xhr.responseText));
-                    }
-                }
-            })
-        }
 
 
-    },
 
-    mounted() {
-        sendRequest(`${API_URL}/catalogData.json`).then(answer =>{
-            this.goods = answer;
-            this.filteredGoods = answer;
-        });
-    }
-});
+/*
+ let butt = document.querySelector('.cartButton');
+ butt.addEventListener('click', () => {
+ sendRequest(`${API_URL}/catalogData.json`).then((phones) => {
+ let phonesString = phones.map(item => `<li>${item.name}: ${item.number}</li>`).join('');
+ document.getElementById('phones').innerHTML = "<ul>" + phonesString + "</ul>";
+ }/!*, onRejected => console.log(onRejected)   *!/)
+ })
 
+
+ // Lesson 5
+
+ let app = new Vue({
+ el: '#app',
+ data: {
+ goods: [],
+ filteredGoods: [],
+ searchLine: '',
+ },
+ methods: {
+
+ sendRequest(url) {
+ return new Promise((resolve, reject) => {
+ let xhr = new XMLHttpRequest();
+ xhr.open('GET', url, true);
+ xhr.send();
+ xhr.onreadystatechange = () => {
+ if (xhr.readyState === 4) {
+ resolve(JSON.parse(xhr.responseText));
+ }
+ }
+ })
+ }
+
+
+ },
+
+ mounted() {
+ sendRequest(`${API_URL}/catalogData.json`).then(answer => {
+ this.goods = answer;
+ this.filteredGoods = answer;
+ });
+ }
+ });
+
+ */
