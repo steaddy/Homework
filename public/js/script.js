1ceac681699f70f7a6cf8b7ce50b7548f5229aee
steaddy <9254497089@mail.ru>
2019-04-09 15:50:48 +0300

    Готовый поиск на Vue. Покупка товаров на Vue - в процессе.

--- public/js/script.js
+++ public/js/script.js
@@ -16,6 +16,7 @@ class Item {
 class ItemList {
     constructor() {
         let items = [];
+        let filteredItems = []; // чтобы не забыть потом сделать поиск товаров
         let total = 0;
     }
 
@@ -36,7 +37,6 @@ class ItemList {
         document.querySelector('.goods').innerHTML = listHtml;
     }
 }
-
 
 
 let itemList = new ItemList;
@@ -60,71 +60,184 @@ function newSendRequest(url) {
     })
 }
 
-
-// Пример с Fetch()
-
-
-let list = document.querySelector('#phones ul');
-
-fetch(`http://localhost:3000/phones.json`)
-    .then((response) => response.json())
-    .then((response) => {
-        list.innerHTML = response.map(item =>
-            `<li>Name: ${item.name}, number: ${item.number}.</li>`
-        ).join('')
-    })
-
-
-
-
-
-
-
-
-
 /*
- let butt = document.querySelector('.cartButton');
- butt.addEventListener('click', () => {
- sendRequest(`${API_URL}/catalogData.json`).then((phones) => {
- let phonesString = phones.map(item => `<li>${item.name}: ${item.number}</li>`).join('');
- document.getElementById('phones').innerHTML = "<ul>" + phonesString + "</ul>";
- }/!*, onRejected => console.log(onRejected)   *!/)
+
+ // Пример с Fetch()
+
+
+ let list = document.querySelector('#phones ul');
+
+ fetch(`http://localhost:3000/users`)
+ .then((response) => response.json())
+ .then((response) => {
+ list.innerHTML = response.map(item =>
+ `<li data-id="${item.id}">Name: ${item.name}, number: ${item.phoneNumber}.</li>` // вставили пользовательский id чтобы по нему удалять элементы списка телефонов
+ ).join('')
  })
 
 
- // Lesson 5
 
- let app = new Vue({
- el: '#app',
- data: {
- goods: [],
- filteredGoods: [],
- searchLine: '',
- },
- methods: {
 
- sendRequest(url) {
- return new Promise((resolve, reject) => {
- let xhr = new XMLHttpRequest();
- xhr.open('GET', url, true);
- xhr.send();
- xhr.onreadystatechange = () => {
- if (xhr.readyState === 4) {
- resolve(JSON.parse(xhr.responseText));
+
+
+
+
+
+
+
+
+
+ // Загрузка на сервер пользователей и их удаление
+
+
+ class User {
+ constructor(name, number, id) {
+ this.id = id;
+ this.name = name;
+ this.number = number;
+ }
+
+ render() {
+ return `<li>${this.name}: ${this.number}</li>`;
  }
  }
+
+ class UsersList {
+ constructor() {
+ this.users = [];
+ this.filteredUsers = [];
+ }
+
+ getUsers(url = `http://localhost:3000/users`) {
+ return fetch(url)
+ .then((response) => response.json())
+ .then((response) => {
+ this.users = response.map(item => new User(item.name, item.phoneNumber, item.id));
+ this.filteredUsers = this.users;
+ console.log(this.filteredUsers);
  })
  }
 
+ addUser(url = `http://localhost:3000/users`, name, number) {
 
- },
-
- mounted() {
- sendRequest(`${API_URL}/catalogData.json`).then(answer => {
- this.goods = answer;
- this.filteredGoods = answer;
- });
  }
+
+ deleteUser(url = `http://localhost:3000/users`, id) {
+
+ }
+
+ filterUsers(query) {
+ let regexp = new RegExp(query, 'i');
+ this.filteredUsers = this.users.filter((item) => regexp.test(item.name));
+ }
+
+ render(elementId) {
+ let element = document.querySelector(`#${elementId}`);
+ element.innerHTML = this.filteredUsers.map(item => item.render()).join('');
+ }
+ }
+
+ // Обрабатываем событие, фильтруем список пользователей на клиентской стороне
+ let searchButton = document.querySelector('#searchbutton');
+ searchButton.addEventListener('click', () => {
+ let searchQuery = document.querySelector('#searchquery').value;
+ newUsers.filterUsers(searchQuery);
+ newUsers.render(`newList`);
  });
 
+ // Получаем и отображаем список пользователей с сервера при загрузке страницы
+ let newUsers = new UsersList();
+ newUsers.getUsers().then(() => {
+ newUsers.render(`newList`);
+ });
+
+
+
+ let uploadButt = document.querySelector('#butt');
+ let forDel = document.querySelector('#phones');
+
+ forDel.addEventListener('click', (event) => {
+ let id = event.target.dataset.id;
+ fetch(`http://localhost:3000/users/${id}`, {
+ method: 'DELETE',
+ }).then(() => {
+ console.log(id);
+ console.log(event.target);
+ event.target.parentElement.removeChild(event.target);
+ });
+ })
+
+
+ uploadButt.addEventListener('click', () => {
+ let name = prompt('Enter your name.');
+ let phone = prompt('Enter your phone.');
+
+ fetch(`http://localhost:3000/users`, {
+ method: 'POST',
+ body: JSON.stringify({name: name, phoneNumber: phone}),
+ headers: {'Content-Type': 'application/json'}
+ }).then((response) => response.json())   // Оказывается POST возвращает в промисе  отправленный нами объект
+ // нужно только узнать он его с сервера берет или нет.
+ .then((user) => {               // через еще один then возвращаем распаршеный объект и отображаем его в html
+ list.innerHTML +=
+ `<li data-id="${user.id}">Name: ${user.name}, number: ${user.phoneNumber}.</li>`;  // вставили пользовательский id чтобы по нему удалять элементы списка телефонов
+ })
+ });
+
+ function usersQuery(url) {
+ return fetch((url), {
+ method: 'GET',
+ }).then((response) => response.json());
+ }
+
+ usersQuery(`http://localhost:3000/users`).then((users) => {
+ console.log(users);
+ });
  */
+
+
+let butt = document.querySelector('.cartButton');
+butt.addEventListener('click', () => {
+    sendRequest(`${API_URL}/catalogData.json`).then((phones) => {
+        let phonesString = phones.map(item => `<li>${item.name}: ${item.number}</li>`).join('');
+        document.getElementById('phones').innerHTML = "<ul>" + phonesString + "</ul>";
+    }/*, onRejected => console.log(onRejected)   */)
+})
+
+
+// Lesson 5
+
+let app1 = new Vue({
+    el: '#app',
+    data: {
+        name: '',
+        goods: ['One good', 'Another good', 'The last one'],
+        filteredGoods: [],
+        searchLine: '',
+    },
+    methods: {
+
+        sendRequest(url) {
+            return new Promise((resolve, reject) => {
+                let xhr = new XMLHttpRequest();
+                xhr.open('GET', url, true);
+                xhr.send();
+                xhr.onreadystatechange = () => {
+                    if (xhr.readyState === 4) {
+                        resolve(JSON.parse(xhr.responseText));
+                    }
+                }
+            })
+        }
+
+
+    },
+
+    mounted() {
+        sendRequest(`${API_URL}/catalogData.json`).then(answer => {
+            this.goods = answer;
+            this.filteredGoods = answer;
+        });
+    }
+});
+
